76498ad10b8230fa748af910b20a605aa09da53b
Papa Informatico <papainformaticomx@gmail.com>
2025-08-18 20:21:39 -0600

    Fix adding

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Message } from './message/message';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Message],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected title = 'slider2';
+  showMessage = false;
+
+  toggleMessage() {
+    this.showMessage = !this.showMessage;
+  }
 }
+
